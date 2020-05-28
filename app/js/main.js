e4ec4faf0640d8aa0b64ebfdc0b0547cb28680b4
gafargits <rajigafaronline@gmail.com>
2020-05-28 10:15:52 +0100

    hamburger and overlay fixed

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,16 @@
-console.log("hello world!")
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+btnHamburger.addEventListener('click', () => {
+  if (header.classList.contains('open')) {
+    header.classList.remove('open')
+    overlay.classList.add('fade-out')
+    overlay.classList.remove('fade-in')
+  } else {
+    header.classList.add('open')
+    overlay.classList.add('fade-in')
+    overlay.classList.remove('fade-out')
+  }
+
+})
